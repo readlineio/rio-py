66067622106c58a811b1120a66053ed959c06acf
Albert Sheu <albert.sheu@gmail.com>
2016-03-10 12:57:34 -0800

    Move http -> https

--- readlineio/constants.py
+++ readlineio/constants.py
@@ -3,7 +3,7 @@ A module for constants used throughout the codebase.
 """
 
 # Location that users will go to in order to see their application
-APPLICATION_BASE_URL = 'http://readline.io'
+APPLICATION_BASE_URL = 'https://readline.io'
 
 # URL of the readline-proxy that is handling the input/output
-CHANNEL_BASE_URL = 'http://readline.io'
+CHANNEL_BASE_URL = 'https://readline.io'
